a94b442089af2848599685e540b6d0e7e96a1df9
Mayank808 <mayankmehra628@gmail.com>
2023-04-19 10:55:11 -0400

    Initial Commit

---
+++ CardScannerKit/CardScannerKit.h
@@ -0,0 +1,18 @@
+//
+//  CardScannerKit.h
+//  CardScannerKit
+//
+//  Created by Mayank Mehra on 2023-04-19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardScannerKit.
+FOUNDATION_EXPORT double CardScannerKitVersionNumber;
+
+//! Project version string for CardScannerKit.
+FOUNDATION_EXPORT const unsigned char CardScannerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardScannerKit/PublicHeader.h>
+
+
